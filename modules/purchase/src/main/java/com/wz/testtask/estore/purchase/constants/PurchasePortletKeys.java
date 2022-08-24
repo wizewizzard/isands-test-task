cabd8a77033ec554e5b7fef46b593c37af02b190
wizewizzard <kingaal93@gmail.com>
2022-08-24 16:06:28 +0700

    Add basic purchase mvc portlet module

---
+++ modules/purchase/src/main/java/com/wz/testtask/estore/purchase/constants/PurchasePortletKeys.java
@@ -0,0 +1,11 @@
+package com.wz.testtask.estore.purchase.constants;
+
+/**
+ * @author aleksei
+ */
+public class PurchasePortletKeys {
+
+	public static final String PURCHASE =
+		"com_wz_testtask_estore_purchase_PurchasePortlet";
+
+}
---
+++ modules/purchase/src/main/java/com/wz/testtask/estore/purchase/portlet/PurchasePortlet.java
@@ -0,0 +1,44 @@
+package com.wz.testtask.estore.purchase.portlet;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.wz.testtask.estore.purchase.constants.PurchasePortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author aleksei
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Purchase",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + PurchasePortletKeys.PURCHASE,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class PurchasePortlet extends MVCPortlet {
+	public void makePurchase(ActionRequest request, ActionResponse response ) throws PortalException {
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(request);
+		long employeeId = ParamUtil.getLong(request, "employeeId");
+		long deviceId = ParamUtil.getLong(request, "deviceId");
+		
+		
+		
+	}
+}
