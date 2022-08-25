118d6228ff785bd5a923549eccd5648a3514fcb2
wizewizzard <kingaal93@gmail.com>
2022-08-25 19:48:38 +0700

    Add best tv sellers portlet
    Add constant

--- modules/best-employee/src/main/java/com/wz/testtask/estore/bestemployee/constants/BestEmployeePortletKeys.java
+++ modules/best-employee/src/main/java/com/wz/testtask/estore/bestemployee/constants/BestEmployeePortletKeys.java
@@ -8,4 +8,7 @@ public class BestEmployeePortletKeys {
 	public static final String BESTEMPLOYEE =
 		"com_wz_testtask_estore_bestemployee_BestEmployeePortlet";
 	
+	public static final String BEST_TV_SELLERS =
+			"com_wz_testtask_estore_bestemployee_BestTvSellersPortlet";
+
 }
