18f2d1b3050483ce0d7defe58cc1f7de8267b241
wizewizzard <kingaal93@gmail.com>
2022-08-25 23:10:22 +0700

    Add base statistics portlet

---
+++ modules/statistics/src/main/java/com/wz/testtask/estore/statistics/constants/StatisticsPortletKeys.java
@@ -0,0 +1,11 @@
+package com.wz.testtask.estore.statistics.constants;
+
+/**
+ * @author aleksei
+ */
+public class StatisticsPortletKeys {
+
+	public static final String STATISTICS =
+		"com_wz_testtask_estore_statistics_StatisticsPortlet";
+
+}
---
+++ modules/statistics/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="statistics.caption"/></b>
+</p>
